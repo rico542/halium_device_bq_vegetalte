51a83c4b36e08b0583727b6f83ad479accea4a91
Rico <rico@alwaysdata.net>
2019-04-28 18:35:41 +0400

    Update kernel.mk

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,5 +3,5 @@ BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
-TARGET_KERNEL_CONFIG := lineageos_vegetalte_defconfig
+TARGET_KERNEL_CONFIG := halium_vegetalte_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
